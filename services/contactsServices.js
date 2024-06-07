796e348b560490a78bd44ed50ada41c0834e7797
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2024-06-07 18:11:43 +0300

    update

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,54 +1,57 @@
 import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
+import Contact from "../models/ContacsModel";
 
-const contactsPath = path.resolve("db", "contacts.json");
+export const getContacts = () => Contact.find();
 
-function arrUpdate(value) {
-  return fs.writeFile(contactsPath, JSON.stringify(value, null, 2));
-}
+// const contactsPath = path.resolve("db", "contacts.json");
 
-export const getContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
-};
+// function arrUpdate(value) {
+//   return fs.writeFile(contactsPath, JSON.stringify(value, null, 2));
+// }
 
-export const getContact = async (id) => {
-  const contacts = await getContacts();
-  const result = contacts.find((contact) => contact.id === id);
-  return result || null;
-};
+// export const getContacts = async () => {
+//   const data = await fs.readFile(contactsPath, "utf-8");
+//   return JSON.parse(data);
+// };
 
-export const addContact = async (data) => {
-  const contacts = await getContacts();
-  const newContact = {
-    id: nanoid(),
-    ...data,
-  };
-  contacts.push(newContact);
-  await arrUpdate(contacts);
-  return newContact;
-};
+// export const getContact = async (id) => {
+//   const contacts = await getContacts();
+//   const result = contacts.find((contact) => contact.id === id);
+//   return result || null;
+// };
 
-export const deleteContact = async (id) => {
-  const contacts = await getContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await arrUpdate(contacts);
-  return result;
-};
+// export const addContact = async (data) => {
+//   const contacts = await getContacts();
+//   const newContact = {
+//     id: nanoid(),
+//     ...data,
+//   };
+//   contacts.push(newContact);
+//   await arrUpdate(contacts);
+//   return newContact;
+// };
 
-export const updateContactById = async (id, data) => {
-  const contacts = await getContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { ...contacts[index], ...data };
-  await arrUpdate(contacts);
+// export const deleteContact = async (id) => {
+//   const contacts = await getContacts();
+//   const index = contacts.findIndex((contact) => contact.id === id);
+//   if (index === -1) {
+//     return null;
+//   }
+//   const [result] = contacts.splice(index, 1);
+//   await arrUpdate(contacts);
+//   return result;
+// };
 
-  return contacts[index];
-};
+// export const updateContactById = async (id, data) => {
+//   const contacts = await getContacts();
+//   const index = contacts.findIndex((contact) => contact.id === id);
+//   if (index === -1) {
+//     return null;
+//   }
+//   contacts[index] = { ...contacts[index], ...data };
+//   await arrUpdate(contacts);
+
+//   return contacts[index];
+// };
